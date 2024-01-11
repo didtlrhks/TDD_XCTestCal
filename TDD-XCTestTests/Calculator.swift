0f5451db73074dc224c4217f5770cc362031c458
sikwan <yysskk99999@naver.com>
2024-01-11 18:55:00 +0900

    Calculator func complete

---
+++ TDD-XCTestTests/Calculator.swift
@@ -0,0 +1,25 @@
+//
+//  Calculator.swift
+//  TDD-XCTest
+//
+//  Created by 양시관 on 1/11/24.
+//
+
+import Foundation
+
+
+class Calculator {
+    func add(_ a : Int, _ b : Int) -> Int {
+        return a + b
+    }
+    func sub(_ a : Int,_ b :Int) -> Int {
+        return a - b
+    }
+    func muliti(_ a :Int, _ b :Int ) -> Int{
+    return a * b
+    }
+    func devide(_ a: Int ,_ b :Int ) -> Int{
+        return a / b
+    }
+    
+}
