d964e2f16b33c16a04f5681f3cd003118417bc11
sikwan <yysskk99999@naver.com>
2024-01-11 19:11:09 +0900

    TDD_XCTestTests complete

--- TDD-XCTestTests/TDD_XCTestTests.swift
+++ TDD-XCTestTests/TDD_XCTestTests.swift
@@ -10,5 +10,32 @@ import XCTest
 
 final class TDD_XCTestTests: XCTestCase {
 
-   
+    func testAdd() {
+        let math = Calculator()
+        
+        let result = math.add(3, 5)
+        XCTAssertEqual(result, 3)
+        
+        
+    }
+    
+    func testsub() {
+        let math = Calculator()
+        let result = math.sub(3, 5)
+        XCTAssertEqual(result,-2)
+        
+    }
+    
+    func testDivide() {
+        let math = Calculator()
+        let result = math.devide(3, 1)
+        XCTAssertEqual(result,-2)
+        
+    }
+    
+    func testmulti() {
+        let math = Calculator()
+        let result = math.muliti(3, 2)
+        XCTAssertEqual(result,6)
+    }
 }
